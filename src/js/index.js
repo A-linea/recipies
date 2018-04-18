9c69dfe369cf5e2950026140c41323d56a988509
Yevhen Dmitrievskiy <ravenio@ex.ua>
2018-04-18 18:27:28 +0300

    search state ws added

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,38 @@
 // Global app controller
-import num from './test';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
 
-const x = 23;
-console.log(`I imported' ${num} 'called js. Variable x is ${x}`);
+/**Global state of the app
+ * Search obj
+ * Current recipe obj
+ * Shopping list obj
+ * Liked recipes
+*/
+const  state = {};
+console.log(state);
+const controlSearch = async()=> {
+  //1. Get a query from the view
+  const query = searchView.getInput();
+  console.log(query);
+
+  if (query) {
+    //2. New search obj and add to state
+    state.search = new Search(query);
+
+    //3. Prepare UI for results
+
+    //4. Search for recipes
+    await state.search.getResults();
+
+    //5. Render results on UI
+    searchView.renderResults(state.search.result)
+
+  }
+};
+
+elements.searchForm.addEventListener('submit', event=> {
+  event.preventDefault();
+  controlSearch();
+});
+
---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '647fdb76dee7ea6991b03060b2153b64';
+    try {
+      const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+    } catch (e) {
+      alert(e);
+    }
+  }
+}
